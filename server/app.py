a8e03ad4254c622ce74aa38f0a09bdeae61a1793
Jensen Lawless <jensenlawless@gmail.com>
2023-07-11 11:59:50 -0600

    finished before review

--- server/app.py
+++ server/app.py
@@ -20,11 +20,119 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
+api = Api(app)
 
-@app.route('/')
-def home():
-    return ''
+class Scientists(Resource):
 
+    def get(self):
+        scientists = Scientist.query.all()
+        scientists_serialized = []
+        for sci in scientists:
+            sci_dict = {
+                "id": sci.id,
+                "name": sci.name,
+                "field_of_study": sci.field_of_study,
+            }
+            scientists_serialized.append(sci_dict)
+            
+        response = make_response(
+            jsonify(scientists_serialized),
+            200
+        )
+
+        return response
+    
+    def post(self):
+
+        data = request.get_json()
+
+        new_sci = Scientist(
+            name=data['name'],
+            field_of_study=data['field_of_study'],
+        )
+
+        db.session.add(new_sci)
+        db.session.commit()
+
+        return make_response(new_sci.to_dict(), 201)
+
+class IndiScientists(Resource):
+
+    def get(self, id):
+        scientist = Scientist.query.filter_by(id=id).first()
+        if scientist:
+            return make_response(scientist.to_dict(), 200)
+        else: 
+           return make_response({"error": "Scientist not found"}) 
+        
+    
+    def patch(self, id):
+        data = request.get_json()
+
+        scientist = Scientist.query.filter_by(id=id).first()
+        for attr in data:
+            setattr(scientist, attr, data[attr])
+        
+        db.session.add(scientist)
+        db.session.commit()
+
+        response = make_response(
+            jsonify(scientist.to_dict()),
+            202
+        )
+
+        return response 
+    
+    def delete(self, id):
+
+        scientist = Scientist.query.filter_by(id=id).first()
+        db.session.delete(scientist)
+        db.session.commit()
+
+        return make_response('', 204)
+
+class Planets(Resource):
+
+    def get(self):
+        planets = Planet.query.all()
+        planets_serialized = []
+        for pla in planets:
+            pla_dict = {
+                "id": pla.id,
+                "name": pla.name,
+                "distance_from_earth": pla.distance_from_earth,
+                "nearest_star": pla.nearest_star,
+            }
+            planets_serialized.append(pla_dict)
+            
+        response = make_response(
+            jsonify(planets_serialized),
+            200
+        )
+
+        return response
+
+class Missions(Resource):
+
+    def post(self):
+        data = request.get_json()
+
+        new_mission = Mission(
+            name=data['name'],
+            scientist_id=data['scientist_id'],
+            planet_id=data['planet_id'],
+        )
+
+        db.session.add(new_mission)
+        db.session.commit()
+
+        return make_response(new_mission.to_dict(), 201)
+
+
+api.add_resource(Missions,  '/missions')   
+api.add_resource(Planets, '/planets')
+api.add_resource(Scientists, '/scientists')
+api.add_resource(IndiScientists, '/scientists/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -25,36 +25,38 @@ class Planet(db.Model, SerializerMixin):
     distance_from_earth = db.Column(db.Integer)
     nearest_star = db.Column(db.String)
 
-    # Add relationship
+    missions = db.relationship('Mission', back_populates='planet')
 
-    # Add serialization rules
+    serialize_rules = ('-missions.planet_id',)
 
 
 class Scientist(db.Model, SerializerMixin):
     __tablename__ = 'scientists'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    field_of_study = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    field_of_study = db.Column(db.String, nullable=False)
 
-    # Add relationship
+    missions = db.relationship('Mission', back_populates='scientist')
 
-    # Add serialization rules
-
-    # Add validation
+    serialize_rules = ('-missions.scientist_id',)
 
 
 class Mission(db.Model, SerializerMixin):
     __tablename__ = 'missions'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
 
-    # Add relationships
+    scientist_id = db.Column(db.Integer, db.ForeignKey('scientists.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=False)
 
-    # Add serialization rules
+    scientist = db.relationship('Scientist', back_populates='missions')
+    planet = db.relationship('Planet', back_populates='missions')
 
-    # Add validation
+
+    serialize_rules = ('-planet.missions', '-scientist.missions',)
+
 
 
 # add any models you may need.
